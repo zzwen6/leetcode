411819a5248ea8599f57b3ffa94c4f3c20027cec
zzwen6 <1104340881@qq.com>
2021-10-27 08:20:33 +0000

    add: 12 数字转罗马表示

---
+++ src/math/Q12.java
@@ -0,0 +1,25 @@
+public class Q12 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().intToRoman(9));
+
+    }
+}
+/**
+ * 数字转为罗马表示
+ * https://leetcode-cn.com/problems/integer-to-roman/
+ */
+class Solution {
+    private String[] roman = {"M", "CM", "D", "CD","C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    public String intToRoman(int num) {
+        StringBuilder builder = new StringBuilder();
+        for (int j = 0; j < nums.length; j++) {
+            int temp = nums[j];
+            while(num >= temp) {
+                builder.append(roman[j]);
+                num -= temp;
+            }
+        }
+        return builder.toString();
+    }
+}
