87c3aa85061488bacc8694bdb17dacc3fcdb3ce3
zzwen6 <1104340881@qq.com>
2021-10-27 09:53:31 +0000

    add: 13 罗马转数字

--- src/math/Q12.java
+++ src/math/Q12.java
@@ -1,25 +1,28 @@
 public class Q12 {
     public static void main(String[] args) {
-        System.out.println(new Solution().intToRoman(9));
+        Q12 s1 = new Q12();
+
+        Solution s = s1.new Solution();
+        System.out.println(s.intToRoman(9));
 
     }
-}
-/**
- * 数字转为罗马表示
- * https://leetcode-cn.com/problems/integer-to-roman/
- */
-class Solution {
-    private String[] roman = {"M", "CM", "D", "CD","C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    private int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    public String intToRoman(int num) {
-        StringBuilder builder = new StringBuilder();
-        for (int j = 0; j < nums.length; j++) {
-            int temp = nums[j];
-            while(num >= temp) {
-                builder.append(roman[j]);
-                num -= temp;
+    /**
+     * 数字转为罗马表示
+     * https://leetcode-cn.com/problems/integer-to-roman/
+     */
+    class Solution {
+        private String[] roman = {"M", "CM", "D", "CD","C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        private int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        public String intToRoman(int num) {
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < nums.length; j++) {
+                int temp = nums[j];
+                while(num >= temp) {
+                    builder.append(roman[j]);
+                    num -= temp;
+                }
             }
+            return builder.toString();
         }
-        return builder.toString();
     }
 }
